a7783077d21f154571e31540ca915cf9dfd09a11
Rishang <rishangbhavsarcs@gmail.com>
2020-12-04 18:13:25 +0530

    clone type install

--- install.sh
+++ install.sh
@@ -1,9 +1,33 @@
 # check if gnome  or not
 if ! [[ $(command -v gnome-shell) ]];then echo "Error: gnome shell not found"; exit 1 ;fi
 
-gnomelooks_path="$HOME/.local/bin/gnomelooks"
+gnomelooks_path="$HOME/.gnomelooks"
+
 ext_path="$HOME/.local/share/gnome-shell/extensions"
 UUID="user-theme@gnome-shell-extensions.gcampax.github.com"
+
+if [[ `command -v apt` ]];then
+    package_installer="apt"
+elif [[ `command -v yum` ]];then
+    package_installer="yum"
+fi
+
+function test_requirement_packages
+{
+    echo -e "Checking if needed packages exist.\n"
+    local needed=("curl" "git" "python3" "pip3" "curl" "gnome-tweaks")
+    
+    for p in ${needed[@]};do
+        if ! [[ `command -v "$p"` ]];then
+            echo "Error: package $p not found."
+            echo -e "\ninstall needed packages. and try again."
+            echo -e "\nRun: sudo $package_installer install curl git python3 python3-pip curl gnome-tweaks"
+            echo 
+            return 1
+        fi
+    done
+}
+
 
 # Install user-theme extension of not present
 function user_theme_ext()
@@ -28,19 +52,35 @@ function user_theme_ext()
 # install gnomelook script
 function gnomelooks_setup()
 {   
-    if ! [ -e "$HOME/.local/bin" ];then mkdir -p "$HOME/.local/bin";fi
+    if ! [ -e "$HOME/.local/bin" ];then
+        mkdir -p "$HOME/.local/bin"
+        echo 'export PATH=$HOME/.local/bin:$PATH' >> $HOME/.bashrc
+    fi
+    
+
     pip3 install $(curl -fsSl "https://raw.githubusercontent.com/Rishang/gnome-theme-installer/master/requirements.txt")
     
-    curl -fsS "https://raw.githubusercontent.com/Rishang/gnome-theme-installer/master/gnomelooks.py" -o $gnomelooks_path
-    chmod +x $gnomelooks_path
+    echo -e "\n Cloaning gnome-theme-installer at ~/.gnomelooks"
+    git clone --depth 1 "https://github.com/Rishang/gnome-theme-installer.git" $gnomelooks_path
+    
+    chmod +x "$gnomelooks_path/gnomelooks.py"
+
+    # soft link
+    echo -e "\n linking gnomelooks.py to ~/.local/bin/gnomelooks"
+    ln -s "$gnomelooks_path/gnomelooks.py" "$HOME/.local/bin/gnomelooks"
+    
+    echo -e "Done\n"
     echo  "Restart terminal and type gnomelooks"
 }
 
-# check if folder for user-theme ext esists
+# check this first, and only then continue
+if ! test_requirement_packages;then exit 1;fi
+
+# check if folder for user-theme ext exist
 if [ -e $ext_path ];then
     user_theme_ext
 else
-    mkdir -p ext_path
+    mkdir -p $ext_path
     user_theme_ext
 fi
 
