5d75713fae5907e82218fa6cc41a05ee02bfee3f
Rishang <rishangbhavsar22@gmail.com>
2021-12-15 10:22:13 +0530

    new looks

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
+.ONESHELL: # Applies to every targets in the file! https://www.gnu.org/software/make/manual/html_node/One-Shell.html
+
 URL="https://www.gnome-look.org/s/Gnome/p/1253385/"
 export TEST_THEME_INSTALLER=true
 
 run:
-	python3 gnomelooks.py -i ${URL}
+	cd looks-install
+	python3 cli.py get ${URL}
 
 sudo_run:
-	sudo python3 gnomelooks.py -i ${URL}
+	cd looks-install
+	sudo python3 cli.py get ${URL}
---
+++ getlooks/looks_path.py
@@ -0,0 +1,51 @@
+
+STATE_PATH = ".config/looks-install/state.json"
+
+DESK_THEME_PATH = {
+    "gnome": {
+        "themes": {
+            "local": "~/.local/share/themes",
+            "global": "/usr/share/themes",
+        },
+        "icons": {
+            "local": "~/.local/share/icons",
+            "global": "/usr/share/icons",
+        },
+        "cursors": {
+            "local": "~/.local/share/icons",
+            "global": "/usr/share/icons",
+        },
+    },
+    "kde": {
+        "themes": {
+            "local": "~/.local/share/plasma/desktoptheme",
+            "global": "/usr/share/plasma/desktoptheme",
+        },
+        "global_theme": {
+            "local": "~/.local/share/plasma/look-and-feel",
+            "global": "/usr/share/plasma/look-and-feel",
+        },
+        "icons": {
+            "local": "~/.local/share/icons",
+            "global": "/usr/share/icons",
+        },
+        "cursors": {
+            "local": "~/.icons",
+            "global": "/usr/share/icons",
+        },
+    },
+    "xfce": {
+        "themes": {
+            "local": "~/.themes",
+            "global": "/usr/share/themes",
+        },
+        "icons": {
+            "local": "~/.icons",
+            "global": "/usr/share/icons",
+        },
+        "cursors": {
+            "local": "~/.icons",
+            "global": "/usr/share/icons",
+        },
+    },
+}
